c0fed1b3d72363d03a07dd837be04729876783f5
Tomek <94tom94@gmail.com>
2018-05-13 22:51:10 +0200

    Gnu source added in define. Problems with invisible PIPE_BUF disappeared

--- Sop2/Lab5/Przygotowanie/fifoServer.c
+++ Sop2/Lab5/Przygotowanie/fifoServer.c
@@ -1,12 +1,4 @@
-// #include <unistd.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <errno.h>
-// #include <sys/types.h>
-// #include <sys/stat.h>
-// #include <fcntl.h>
-// #include <ctype.h>
-
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
